c65cd7f06e30992ea6a4e7892cf6d69f46553ad0
linostar <linux.anas@gmail.com>
2015-08-13 14:30:56 +0300

    Lowercase for all keys in the data json

--- aidelib/data_processing.py
+++ aidelib/data_processing.py
@@ -15,3 +15,13 @@ class Data:
 			sys.exit(1)
 		with open(self.DATA_JSON_FILE, 'r') as data_fp:
 			self.topics = json.load(data_fp)
+			self.topics = self.lower_keys(self.topics)
+
+	def lower_keys(self, d):
+		new_d = {}
+		for key, val in d.items():
+			if isinstance(d[key], dict):
+				new_d[str(key).lower()] = self.lower_keys(d[key])
+			else:
+				new_d[str(key).lower()] = val
+		return new_d
